6447ecb8538a83b9250737dc0668d1a0abf3fd4a
You-jeong136 <smj06168@gmail.com>
2023-04-14 15:24:31 +0900

    롤케이크 자르기

---
+++ programmers/cuttingRollcake.kt
@@ -0,0 +1,27 @@
+
+fun main(args : Array<String>){
+    val topping : IntArray = intArrayOf(1, 2, 1, 3, 1, 4, 1, 2)
+    print(solution(topping))
+}
+fun solution(topping: IntArray): Int {
+    var answer: Int = 0
+
+    val a = HashMap<Int, Int>()
+    val b = HashMap<Int, Int>()
+    for (top in topping) b[top] = b.getOrDefault(top, 0) + 1
+
+    for (i in topping.indices) {
+        a[topping[i]] = a.getOrDefault(topping[i], 0) + 1
+        b[topping[i]] = b.getOrDefault(topping[i], 0) - 1
+        if (b.getOrDefault(topping[i], 0) == 0){
+            b.remove(topping[i])
+        }
+        if (a.size == b.size) {
+            answer++
+        }
+    }
+    return answer
+
+}
+
+
